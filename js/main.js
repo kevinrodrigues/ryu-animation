8ed86c25b34282e12ad6a17eb133a2c382ff24a8
Kevin Rodrigues <kevrodrigues116@gmail.com>
2015-05-29 09:05:01 +0100

    Added ryu to stage.

--- js/main.js
+++ js/main.js
@@ -5,10 +5,8 @@
 		ryu;
 
 
-		ryuImage = new Image();
 
-		ryuImage.src = 'img/sprite.png';
-
+		
 
 	function sprite(options) {
 		var that = {};
@@ -18,21 +16,56 @@
 		that.height = options.height;
 		that.image = options.image;
 
+		// context.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh)
+		
+		that.render = function() {
+
+			// console.log(that.image);
+			//draw animation..
+			that.context.drawImage(
+			that.image,
+			0,
+			0,
+			that.width,
+			that.height,
+			0,
+			0,
+			that.width,
+			that.height);
+		};
+
+
+		// console.log(that);
+
 		return that;
+
 	}
 
 	// Get canvas
 	canvas = document.getElementById('ryu');
-	canvas.width = 100;
-	canvas.height = 100;
+	canvas.width = 120;
+	canvas.height = 92;
 
+
+	ryuImage = new Image();
+
+	ryuImage.src = 'img/sprite.png';
 	//create sprite
 	ryu = sprite({
 	    context: canvas.getContext('2d'),
-	    width: 100,
-	    height: 100,
+	    width: 120,
+	    height: 92,
 	    image: ryuImage
 	});
+
+	// console.log(ryu);
+
+	function start() {
+		ryu.render();
+	}
+	
+
+	ryuImage.addEventListener('load', start);
 
 }());
 
